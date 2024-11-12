2da8ff1b6ff4a718cd3afc381682ab37a125b495
Rahul Mandal <rmrahul258@gmail.com>
2024-11-12 22:48:52 +0530

    Day 119

---
+++ Stack  & Queue/1544. Make The String Great.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        stack<char> st;
+
+        for(char c : s) 
+        {
+            if(!st.empty() && (st.top() == c - 32 || st.top() == c + 32))
+                st.pop();
+            else
+                st.push(c);
+        }
+
+        string res;
+        while(!st.empty())
+        {
+            res.push_back(st.top());
+            st.pop();
+        }
+
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
