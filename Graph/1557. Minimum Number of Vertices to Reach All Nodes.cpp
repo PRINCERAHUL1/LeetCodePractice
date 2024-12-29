a7cf718a1ec99940665630551c980a8e288085e0
Rahul Mandal <rmrahul258@gmail.com>
2024-12-29 21:48:29 +0530

    Day 167

---
+++ Graph/1557. Minimum Number of Vertices to Reach All Nodes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
+        vector<int> node(n, 0);
+
+        for(int i=0; i<edges.size(); i++)
+            node[edges[i][1]]++;
+        
+        vector<int> res;
+
+        for(int i=0; i<n; i++)
+        {
+            if(node[i] == 0)
+                res.push_back(i);
+        }
+
+        return res;
+    }
+};
