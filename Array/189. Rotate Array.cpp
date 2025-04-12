0b77720363d61e23acdd6250fa028df4be397d72
Rahul Mandal <rmrahul258@gmail.com>
2025-04-12 23:10:27 +0530

    Day 271

---
+++ Array/189. Rotate Array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k %= nums.size();
+
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin()+k);
+        reverse(nums.begin()+k, nums.end());
+    }
+};
+
+//class Solution {
+//public:
+//    void rotate(vector<int>& nums, int k) {
+//        int n = nums.size();
+//        k = k % n;
+//        vector<int> temp(n);
+//
+//        for(int i=0; i<n; i++)
+//            temp[(i+k) % n] = nums[i];
+//        
+//        nums = temp;
+//    }
+//};
