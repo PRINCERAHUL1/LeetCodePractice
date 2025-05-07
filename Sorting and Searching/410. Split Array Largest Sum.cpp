1da5ab5a9df962def41e14fc839a2461a6f0cb3c
Rahul Mandal <rmrahul258@gmail.com>
2025-05-07 22:17:55 +0530

    Day 296

---
+++ Sorting and Searching/410. Split Array Largest Sum.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+
+    bool isFeasible(vector<int>& nums, int maxSum, int k)
+    {
+        int count = 1;
+        int currentSum = 0;
+        
+        for (int num : nums)
+        {
+            currentSum += num;
+            if (currentSum > maxSum)
+            {
+                count++;
+                currentSum = num;
+                
+                if (count > k)
+                    return false;
+            }
+        }
+        
+        return true;
+    }
+
+    int splitArray(vector<int>& nums, int k) {
+        int low = 0;
+        int high = 0;
+        
+        for (int num : nums)
+        {
+            low = max(low, num);
+            high += num;
+        }
+
+        int ans = high;
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            
+            if (isFeasible(nums, mid, k))
+            {
+                ans = mid;
+                high = mid - 1;
+            }
+            else
+                low = mid + 1;
+        }
+
+        return ans;
+    }
+};
